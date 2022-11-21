abccfbef681723a86a8dcd6fdf7bd76bf54f2902
sting7401 <sting7401@naver.com>
2022-11-21 11:28:13 +0900

    interface

--- app.ts
+++ app.ts
@@ -1,30 +1,30 @@
-type Store = {
+interface Store {
     currentPage: number;
     feeds: NewsFeed[];
 }
 
-type News = {
-    id: number;
-    title: string;
-    time_ago: string;
-    url: string;
-    user: string;
-    content: string;
+interface News {
+    readonly id: number;
+    readonly title: string;
+    readonly time_ago: string;
+    readonly url: string;
+    readonly user: string;
+    readonly content: string;
 }
 
-type NewsFeed = News & {
-    comments_count: number;
-    points: number;
+interface NewsFeed extends News {
+    readonly comments_count: number;
+    readonly points: number;
     read?: boolean;
 }
 
-type NewsDetail = News &  {
-    comments: NewsComment[];
+interface NewsDetail extends News {
+    readonly comments: NewsComment[];
 }
 
-type NewsComment = News &  {
-    comments : NewsComment[];
-    level: number;
+interface NewsComment extends News {
+    readonly comments : NewsComment[];
+    readonly level: number;
 }
 
 const container : HTMLElement | null = document.getElementById('root');
@@ -44,7 +44,7 @@ function getData<AjaxResponse>(url : string) : AjaxResponse {
     return JSON.parse(ajax.response);
 }
 
-function makeFeeds(feeds : NewsFeed[]) {
+function makeFeeds(feeds : NewsFeed[]): NewsFeed[] {
     for (let i = 0; i < feeds.length; i+=1) {
         feeds[i].read = false;
     }
